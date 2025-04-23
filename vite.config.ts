1a58c19c99d46de50c4610f622708df94da4d4de
Anjulsinghal <anjulsinghal123@gmail.com>
2025-04-23 16:06:13 +0530

    service worker added...

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), VitePWA({
     registerType: 'prompt',
-    injectRegister: false,
+    injectRegister: 'auto',
 
     pwaAssets: {
       disabled: false,
