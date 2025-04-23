d3fd17fb1f45200249acd8e8d1c4dcaa81da1923
Anjulsinghal <anjulsinghal123@gmail.com>
2025-04-23 16:13:16 +0530

    fix..

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+    injectRegister: false,
 
     pwaAssets: {
       disabled: false,
@@ -21,6 +21,7 @@ export default defineConfig({
     },
 
     workbox: {
+      // importWorkbox: true,
       globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
